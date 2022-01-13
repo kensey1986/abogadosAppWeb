656d34d98e2803c46156af88399841d55e26307c
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-01-13 09:25:34 -0500

    navbar-mobil-80%

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -32,22 +32,25 @@ export const BarNav = () => {
                 <Navbar.Toggle className='icon-menu' aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse  id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Nav.Link href="#features">INICIO</Nav.Link>
+                        <NavDropdown title="SERVICIOS" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Servicio 1</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Servicio 2</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Servicio 3</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Servicio 4</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
+                        <Nav.Link href="#pricing">QUIENES SOMOS</Nav.Link>
+                        <Nav.Link href="#pricing">NUESTROS PROFESIONALES</Nav.Link>
+
                     </Nav>
-                    <Nav>
+                    {/* <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
                             Dank memes
                         </Nav.Link>
-                    </Nav>
+                    </Nav> */}
                 </Navbar.Collapse>
             </Container>
         </Navbar>
