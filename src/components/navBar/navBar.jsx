5c811de6056aba312c8b65cfdea700e5d8e69fb2
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-01-13 15:46:37 -0500

    media query 375px -navbar-100%

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -15,45 +15,44 @@ export const BarNav = () => {
         <Navbar className='navBar' collapseOnSelect expand="lg" variant="dark">
             <Container>
                 <Row className='container-logo'>
-                    <Col className='container-parte-1'>
+                    <Col className='container-logo-parte-1'>
                         <Image
                             className="logo"
                             src={logo}
                             alt="Garcia & Asociados"
                         />
                     </Col>
-                    <Col className='container-parte-2' >
+                    <Col className='container-logo-parte-2' >
                         <h6>COLECTIVO DE ABOGADOS.</h6>
                         <h1> <strong> Garcia & Asociados. </strong></h1>
                     </Col>
                 </Row>
-            
-                <Row>
+                <Row className='container-menu'>
                     <Col>
-                <Navbar.Toggle className='icon-menu' aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse  id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#features">INICIO</Nav.Link>
-                        <NavDropdown title="SERVICIOS" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Servicio 1</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Servicio 2</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Servicio 3</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">Servicio 4</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#pricing">QUIENES SOMOS</Nav.Link>
-                        <Nav.Link href="#pricing">NUESTROS PROFESIONALES</Nav.Link>
+                        <Navbar.Toggle className='icon-menu' aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="#features">INICIO</Nav.Link>
+                                <NavDropdown title="SERVICIOS" id="collasible-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Servicio 1</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Servicio 2</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Servicio 3</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.4">Servicio 4</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="#pricing">QUIENES SOMOS</Nav.Link>
+                                <Nav.Link href="#pricing">NUESTROS PROFESIONALES</Nav.Link>
 
-                    </Nav>
-                    {/* <Nav>
+                            </Nav>
+                            {/* <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
                         <Nav.Link eventKey={2} href="#memes">
                             Dank memes
                         </Nav.Link>
                     </Nav> */}
-                </Navbar.Collapse>
-                </Col>
+                        </Navbar.Collapse>
+                    </Col>
                 </Row>
             </Container>
         </Navbar>
